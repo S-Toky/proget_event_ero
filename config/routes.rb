5c0ca15cc96a98d3d5b2b568027b20ebdf2d62bc
S-Toky <stokyr29@gmail.com>
2019-08-14 16:10:12 +0300

    add first

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/show'
+ 	root to: 'events#index'
+  devise_for :users
+  resources :users, only:[:show]
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
